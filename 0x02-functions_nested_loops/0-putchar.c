d4ee40034384f4f76667d0fdab72017eba628132
Yatich-Winston <yatichwinston@gmail.com>
2023-10-11 18:37:22 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*main - this is a function
+*Return:return(0);
+*/
+int main(void)
+{
+
+_putchar.c('_');
+_putchar.c('p');
+_putchar.c('u');
+_putchar.c('t');
+_putchar.c('c');
+_putchar.c('h');
+_putchar.c('a');
+_putchar.c('r');
+_putchar.c('\n');
+
+return (0);
+}
